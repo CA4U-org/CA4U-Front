66e46360d9c3802ab1b90db4ef5101748233c5f1
seobeomseok <sbslc2000@gmail.com>
2024-10-10 16:27:15 +0900

    feat: add more design on branding

--- src/MobileFrameWrapper.jsx
+++ src/MobileFrameWrapper.jsx
@@ -6,6 +6,10 @@ function Branding() {
         <Flex
             direction={'column'}
             align={'center'}
+            bg={'white'}
+            p={8}
+            borderRadius={'lg'}
+            boxShadow={'lg'}
         >
             <Image src={CA4UIcon} w={'100px'} h={'44px'} />
             <Text
@@ -17,14 +21,14 @@ function Branding() {
             >
                 Club & Academy <span style={{color:'red'}}>4</span> U
             </Text>
-            <Text mt={1} fontSize={'sm'} color={'gray'}>동아리가 당신의 손안에</Text>
+            <Text mt={1} fontSize={'sm'} color={'gray.800'}>중앙대 동아리가 당신의 손안에</Text>
         </Flex>
     )
 }
 export function MobileFrameWrapper(props) {
     return (
-        <Flex bg={'#eee'} w={'100vw'} direction={'row'}>
-            <Flex m={'0 auto'} direction={'row'}>
+        <Flex bg={'#eee'} w={'100vw'} className={'no-drag'}>
+            <Flex m={'0 auto'}>
                 <Show above={'sm'}>
                     <Flex w={'sm'} align={'center'} justify={'center'} direction={'column'}>
                         <Branding />
@@ -35,7 +39,6 @@ export function MobileFrameWrapper(props) {
                     h={'100vh'}
                     overflowY={'scroll'}
                     bgColor={'white'}
-                    className={'no-drag'}
                 >
                     {/*이 곳에 애플리케이션 화면이 들어옵니다.*/}
                     {props.children}
