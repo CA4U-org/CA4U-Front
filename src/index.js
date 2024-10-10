e978875847f71dfa629eb2a05a2ed74fda811657
seobeomseok <sbslc2000@gmail.com>
2024-10-10 14:48:34 +0900

    feat: add ChakraProvider in index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-// index.js 또는 App.js 상단에 추가
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import "./index.css";
 import App from "./App";
-import ImageSlider from "./components/ImageSlider";
-import MainPage from "./MainPage";
-import ClubDetailPage from "./ClubDetailPage";
-import FavoriteButton from "./components/FavoriteButton";
-import VerifiedButton from "./components/VerifiedButton";
-import ClubInfo from "./components/ClubInfo";
-import ClubBoardPage from "./components/ClubBoardPage";
-import PostDetailPage from "./PostDetailPage";
-import ClubVerificationPage from "./ClubVerificationPage";
+import {ChakraProvider} from "@chakra-ui/react";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+      <ChakraProvider>
+          <App />
+      </ChakraProvider>
   </React.StrictMode>
 );
