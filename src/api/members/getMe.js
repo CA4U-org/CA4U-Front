2f8f4992a35997ed7daae3a7b25eb842153c6f92
seobeomseok <sbslc2000@gmail.com>
2024-11-06 00:23:08 +0900

    feat: add withCredentials in axios request

--- src/api/members/getMe.js
+++ src/api/members/getMe.js
@@ -4,7 +4,9 @@ import { SERVER_URL } from '../../shared/const/consts';
 
 export async function getMe() {
   try {
-    const res = await axios.get(`${SERVER_URL}/api/v1/users/me`);
+    const res = await axios.get(`${SERVER_URL}/api/v1/users/me`, {
+      withCredentials: true, // 올바른 옵션으로 수정
+    });
     return res.data;
   } catch (err) {
     return err.response.data;
